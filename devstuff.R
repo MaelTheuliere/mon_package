71f80f2b6ebb4a7ba2e042edfe539f8d5345a78b
maeltheuliere <mael.theuliere@insee.fr>
2023-02-27 17:37:02 +0000

    ajout pkgdown

--- devstuff.R
+++ devstuff.R
@@ -13,6 +13,13 @@ usethis::use_build_ignore('.lintr')
 usethis::use_r("celsius_to_kelvin")
 usethis::use_package('assertthat')
 usethis::use_test("celsius_to_kelvin")
+# CI GHA
+usethis::use_github_action_check_standard()
+usethis::use_github_action("lint")
+# pkgdown
+usethis::use_readme_rmd()
+usethis::use_pkgdown()
+usethis::use_github_action("pkgdown")
 # verification lintage
 lintr::lint_package()
 # Gestion du style
